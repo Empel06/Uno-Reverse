022bbf63207210084f8e8fef992678daa2b26d45
Emiel Mangelschots <12302093@student.pxl.be>
2024-05-22 21:34:21 +0200

    threads voor het verzenden van gegevens naar de client, Includen van pthread.h

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
     #include <stdlib.h>
     #include <string.h>
     #include <unistd.h>
+    #include <pthread.h>
 
     void OSInit(void) {
         WSADATA wsaData;
@@ -32,6 +33,7 @@
     #include <unistd.h>
     #include <stdlib.h>
     #include <string.h>
+    #include <pthread.h>
 
     void OSInit(void) {}
     void OSCleanup(void) {}
@@ -42,7 +44,7 @@
 
 int initialization();
 int connection(int internet_socket);
-void execution(int internet_socket);
+void execution(int client_internet_socket);
 void cleanup(int internet_socket, int client_internet_socket);
 void log_message(const char *client_ip, int sendcount);
 
@@ -71,187 +73,99 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-int initialization()
-{
-	//Step 1.1
-	
+int initialization() {
+    // Step 1.1
+    struct addrinfo internet_address_setup;
+    struct addrinfo *internet_address_result;
+    memset(&internet_address_setup, 0, sizeof internet_address_setup);
+    internet_address_setup.ai_family = AF_UNSPEC;
+    internet_address_setup.ai_socktype = SOCK_STREAM;
+    internet_address_setup.ai_flags = AI_PASSIVE;
+    int getaddrinfo_return = getaddrinfo(NULL, "22", &internet_address_setup, &internet_address_result);
+    if (getaddrinfo_return != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(getaddrinfo_return));
+        exit(1);
+    }
 
+    int internet_socket = -1;
+    struct addrinfo *internet_address_result_iterator = internet_address_result;
+    while (internet_address_result_iterator != NULL) {
+        // Step 1.2
+        internet_socket = socket(internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol);
+        if (internet_socket == -1) {
+            perror("socket error");
+        } else {
+            // Step 1.3
+            int bind_return = bind(internet_socket, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen);
+            if (bind_return == -1) {
+                perror("bind error");
+                close(internet_socket);
+            } else {
+                // Step 1.4
+                int listen_return = listen(internet_socket, 1);
+                if (listen_return == -1) {
+                    close(internet_socket);
+                    perror("listen error");
+                } else {
+                    break;
+                }
+            }
+        }
+        internet_address_result_iterator = internet_address_result_iterator->ai_next;
+    }
 
+    freeaddrinfo(internet_address_result);
 
-	struct addrinfo internet_address_setup;
-	struct addrinfo * internet_address_result;
-	memset( &internet_address_setup, 0, sizeof internet_address_setup );
-	internet_address_setup.ai_family = AF_UNSPEC;
-	internet_address_setup.ai_socktype = SOCK_STREAM;
-	internet_address_setup.ai_flags = AI_PASSIVE;
-	int getaddrinfo_return = getaddrinfo( "::1", "22", &internet_address_setup, &internet_address_result );
-	if( getaddrinfo_return != 0 )
-	{
-		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
-		exit( 1 );
-	}
+    if (internet_socket == -1) {
+        fprintf(stderr, "socket: geen adres gevonden\n");
+        exit(2);
+    }
 
-	int internet_socket = -1;
-	struct addrinfo * internet_address_result_iterator = internet_address_result;
-	while( internet_address_result_iterator != NULL )
-	{
-		//Step 1.2
-		internet_socket = socket( internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol );
-		if( internet_socket == -1 )
-		{
-			perror( "socket error" );
-		}
-		else
-		{
-			//Step 1.3
-			int bind_return = bind( internet_socket, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
-			if( bind_return == -1 )
-			{
-				perror( "bind error" );
-				close( internet_socket );
-			}
-			else
-			{
-				//Step 1.4
-				int listen_return = listen( internet_socket, 1 );
-				if( listen_return == -1 )
-				{
-					close( internet_socket );
-					perror( "listen error" );
-				}
-				else
-				{
-					break;
-				}
-			}
-		}
-		internet_address_result_iterator = internet_address_result_iterator->ai_next;
-	}
-
-	freeaddrinfo( internet_address_result );
-
-	if( internet_socket == -1 )
-	{
-		fprintf( stderr, "socket: geen adres gevonden\n" );
-		exit( 2 );
-	}
-
-	return internet_socket;
+    return internet_socket;
 }
 
-int connection( int internet_socket )
-{
-	//Step 2.1
-	struct sockaddr_storage client_internet_address;
-	socklen_t client_internet_address_length = sizeof client_internet_address;
-	int client_socket = accept( internet_socket, (struct sockaddr *) &client_internet_address, &client_internet_address_length );
-	if( client_socket == -1 )
-	{
-		perror( "accept error" );
-		close( internet_socket );
-		exit( 3 );
-	} 
+int connection(int internet_socket) {
+    // Step 2.1
+    struct sockaddr_storage client_internet_address;
+    socklen_t client_internet_address_length = sizeof(client_internet_address);
+    int client_socket = accept(internet_socket, (struct sockaddr *)&client_internet_address, &client_internet_address_length);
+    if (client_socket == -1) {
+        perror("accept error");
+        close(internet_socket);
+        exit(3);
+    }
 
-			//finding out the ip address
-			char client_ip[INET6_ADDRSTRLEN]; // This can accommodate both IPv4 and IPv6 addresses
-			if (client_internet_address.ss_family == AF_INET)
-			{
-				struct sockaddr_in *s = (struct sockaddr_in *)&client_internet_address;
-				inet_ntop(AF_INET, &(s->sin_addr), client_ip, sizeof client_ip);
-			}
-			else if (client_internet_address.ss_family == AF_INET6)
-			{
-				struct sockaddr_in6 *s = (struct sockaddr_in6 *)&client_internet_address;
-				inet_ntop(AF_INET6, &(s->sin6_addr), client_ip, sizeof client_ip);
-			}
-			else
-			{
-				fprintf(stderr, "geen herkend IPadres\n");
-				close(client_socket);
-				close(internet_socket);
-				exit(4);
-			}
+    // Finding out the IP address
+    char client_ip[INET6_ADDRSTRLEN]; // This can accommodate both IPv4 and IPv6 addresses
+    if (client_internet_address.ss_family == AF_INET) {
+        struct sockaddr_in *s = (struct sockaddr_in *)&client_internet_address;
+        inet_ntop(AF_INET, &(s->sin_addr), client_ip, sizeof(client_ip));
+    } else if (client_internet_address.ss_family == AF_INET6) {
+        struct sockaddr_in6 *s = (struct sockaddr_in6 *)&client_internet_address;
+        inet_ntop(AF_INET6, &(s->sin6_addr), client_ip, sizeof(client_ip));
+    } else {
+        fprintf(stderr, "Unrecognized IP address family\n");
+        close(client_socket);
+        close(internet_socket);
+        exit(4);
+    }
 
-			printf("verbonden IPadres: %s\n", client_ip);
-			#ifndef debug
-			strcpy(ip_lookup, client_ip);
-			#endif
+    printf("Connected IP address: %s\n", client_ip);
 
-			char BigLine[] = "+---------------------------------------------------+\n";
-			char logbuffer[1000];
-			FILE *logp;
-			logp = fopen("IPLOG.txt", "a");
-			if(logp != NULL){
-				snprintf(logbuffer, sizeof(logbuffer), "%sIPadres attacker: %s\n", BigLine, ip_lookup);
-				fprintf(logp, logbuffer);
-			}
-			fclose(logp);
+    // Log the IP address
+    FILE *logp = fopen("IPLOG.txt", "a");
+    if (logp != NULL) {
+        fprintf(logp, "Connected IP address: %s\n", client_ip);
+        fclose(logp);
+    }
 
-			char CLI_buffer[1000];
-			snprintf(CLI_buffer, sizeof(CLI_buffer),"curl http://ip-api.com/json/%s?fields=1561", ip_lookup);
-			FILE *fp;
-    		char IP_LOG_ITEM[2000];
+    // Optionally, you can add geolocation or other processing here
 
-    		fp = popen(CLI_buffer, "r");
-    		if (fp == NULL) {
-        	printf("Error cli\n");
-        	return client_socket;
-    		}
-			fgets(IP_LOG_ITEM, sizeof(IP_LOG_ITEM)-1, fp);
-			
-			system("clear");
-			printf(logbuffer);
-			printf("%s\n", IP_LOG_ITEM);
-
-    		pclose(fp);
-            
-            if(IP_LOG_ITEM[1] != '}'){
-			char country[50];
-			char regionName[50];
-			char city[50];
-			char isp[50];
-			char org[50];
-				
-            //Parsing
-			logp = fopen("IPLOG.txt", "a");
-			if(logp != NULL){
-				char logbuffer[1000];
-				for (int i = 0; IP_LOG_ITEM[i] != '\0'; i++)
-			{
-				if(IP_LOG_ITEM[i]== ','){
-				fprintf(logp, "\n");
-				printf("\n");
-				}else if(IP_LOG_ITEM[i] == ':'){
-				fprintf(logp, ": ");
-				printf(": ");
-				}else if(IP_LOG_ITEM[i] == '{' || IP_LOG_ITEM[i] == '}'){
-				}else if(IP_LOG_ITEM[i] == '"' || IP_LOG_ITEM[i] == '"'){
-				}else{
-				fprintf(logp, "%c", IP_LOG_ITEM[i]);
-				printf("%c", IP_LOG_ITEM[i]);
-				}
-
-				
-			}
-			fprintf(logp, "\n");
-			 printf("\n");
-				
-			}
-			fclose(logp);
-			}else{
-			FILE *logp;
-			logp = fopen("IPLOG.txt", "a");
-			if(logp != NULL){
-				fprintf(logp, "LocalHost, Geen geoloc mogelijk\n");
-			}
-			fclose(logp);
-			}
-
-
-		return client_socket;
+    return client_socket;
 }
 
-void execution(int internet_socket) {
+void* send_data(void* arg) {
+    int client_internet_socket = *(int*)arg;
     char chartosend[] = 
         "Ha, jij bent vies maar ik doe gemener\n"
         "In de club, kom je moeder tegen\n"
@@ -286,7 +200,7 @@ void execution(int internet_socket) {
 #else
     for (int i = 0; i < 1000; i++) {
 #endif
-        int number_of_bytes_send = send(internet_socket, chartosend, strlen(chartosend), 0);
+        int number_of_bytes_send = send(client_internet_socket, chartosend, strlen(chartosend), 0);
         if (number_of_bytes_send == -1) {
             perror("send error");
             break;
@@ -300,10 +214,18 @@ void execution(int internet_socket) {
     // Log IP address and number of sent messages
     struct sockaddr_in addr;
     socklen_t addr_len = sizeof(addr);
-    getsockname(internet_socket, (struct sockaddr *)&addr, &addr_len);
+    getsockname(client_internet_socket, (struct sockaddr *)&addr, &addr_len);
     char ip_address[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(addr.sin_addr), ip_address, INET_ADDRSTRLEN);
     log_message(ip_address, sendcount);
+
+    return NULL;
+}
+
+void execution(int client_internet_socket) {
+    pthread_t send_thread;
+    pthread_create(&send_thread, NULL, send_data, &client_internet_socket);
+    pthread_join(send_thread, NULL);
 }
 
 void log_message(const char *client_ip, int sendcount) {
